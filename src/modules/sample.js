5f4e9ef9ef2f21a1efbdb0a6dda0702bc8490d6d
Danbee Kim <kdb2360@gmail.com>
2022-11-29 10:52:10 +0900

    리팩토링

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -1,5 +1,6 @@
 import { handleActions } from "redux-actions";
 import * as api from "../lib/api";
+import createRequestThunk from "../lib/createRequestThunk";
 
 //타입 액션을 선언
 const GET_POST = "sample/GET_POST";
@@ -11,41 +12,8 @@ const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
 const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
 
 //thunk 함수 생성
-export const getPost = (id) => async (dispatch) => {
-  dispatch({ type: GET_POST });
-  try {
-    const response = await api.getPost(id);
-    dispatch({
-      type: GET_POST_SUCCESS,
-      payload: response.data,
-    });
-  } catch (e) {
-    dispatch({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true,
-    });
-    throw e; //나중에 컴포넌트단에서  에러를 조회할 수 있게 해 줌
-  }
-};
-
-export const getUsers = () => async (dispatch) => {
-  dispatch({ type: GET_USERS });
-  try {
-    const response = await api.getUsers();
-    dispatch({
-      type: GET_USERS_SUCCESS,
-      payload: response.data,
-    });
-  } catch (e) {
-    dispatch({
-      type: GET_USERS_FAILURE,
-      payload: e,
-      error: true,
-    });
-    throw e; //나중에 컴포넌트단에서  에러를 조회할 수 있게 해 줌
-  }
-};
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 //초기 상태 선언
 const initialState = {
