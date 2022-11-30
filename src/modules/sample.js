b73a9b8f80b015eb88fa1b83e20f7100d46487f9
Danbee Kim <kdb2360@gmail.com>
2022-11-30 10:40:27 +0900

    리팩토링

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -5,11 +5,9 @@ import createRequestThunk from "../lib/createRequestThunk";
 //타입 액션을 선언
 const GET_POST = "sample/GET_POST";
 const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
-const GET_POST_FAILURE = "sample/GET_POST_FAILURE";
 
 const GET_USERS = "sample/GET_USERS";
 const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
-const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
 
 //thunk 함수 생성
 export const getPost = createRequestThunk(GET_POST, api.getPost);
@@ -17,65 +15,20 @@ export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 //초기 상태 선언
 const initialState = {
-  loading: {
-    GET_POST: false,
-    GET_USERS: false,
-  },
   post: null,
   users: null,
 };
 
 const sample = handleActions(
   {
-    [GET_POST]: (state) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: true, //요청 시작
-      },
-    }),
     [GET_POST_SUCCESS]: (state, action) => ({
       ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: false, //요청 완료
-      },
       post: action.payload,
     }),
-    [GET_POST_FAILURE]: (state, action) => ({
-      ...state,
-      loading: {
-        ...state,
-        loading: {
-          ...state.loading,
-          GET_POST: false, //요청 완료
-        },
-      },
-    }),
-    [GET_USERS]: (state) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_USERS: true, //요청 시작
-      },
-    }),
+
     [GET_USERS_SUCCESS]: (state, action) => ({
       ...state,
-      loading: {
-        ...state.loading,
-        GET_USERS: false, //요청 완료
-      },
       users: action.payload,
-    }),
-    [GET_USERS_FAILURE]: (state, action) => ({
-      ...state,
-      loading: {
-        ...state,
-        loading: {
-          ...state.loading,
-          GET_USERS: false, //요청 완료
-        },
-      },
     }),
   },
   initialState,
